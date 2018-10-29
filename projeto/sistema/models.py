fa79e51eb768289f8f8c058d260895211c366fed
rsaless <rspavarina@gmail.com>
2018-10-29 13:46:49 -0300

    criando o novo model

--- projeto/sistema/models.py
+++ projeto/sistema/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100, blank=False)
+    ra = models.PositiveIntegerField(default=123456, blank=False)
+    trancou = models.BooleanField(null=True, blank=True, default=False)
+
+    def __str__(self):
+        return self.nome
+

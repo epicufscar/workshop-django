d02b2e670da9d6f64ce4125f999fe683eab6d727
rsaless <rspavarina@gmail.com>
2018-10-29 15:41:26 -0300

    nova rota

--- projeto/sistema/views.py
+++ projeto/sistema/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Aluno
 
 
@@ -6,3 +6,16 @@ def home(request):
     alunos = Aluno.objects.all()
     return render(request, 'homt.html',{'alunos':alunos})
 
+def adicionar(request):
+
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        ra = request.POST.get('ra')
+        aluno = Aluno.objects.create()
+        aluno.nome = nome
+        aluno.ra = ra
+        aluno.trancou = False
+        aluno.save()
+        return redirect('home')
+
+    return render(request, 'adicionar.html', {})
